20b7b5806311f9ec1322588511fd1ab9b3d43652
Nathalia Bruno <nathi.bruno@gmail.com>
2023-03-06 00:25:05 -0300

    Add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,9 @@
 import Form from "./Form"
 import Text from "./Text"
-import TopBar from "./TopBar"
 import ContentWrapper from "./ContentWrapper"
-import styled from "styled-components"
 
 import { useState } from "react"
 
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  max-width: 100vw;
-  padding: 5rem;
-  box-sizing: border-box;
-  position: relative;
-`
 const App = () => {
   const [submitted, setSubmitted] = useState(false)
 
@@ -23,15 +11,12 @@ const App = () => {
     setSubmitted(value)
   }
   return (
-    <Main>
-      <TopBar />
-      <ContentWrapper>
-        <Text variant="h1" align="center" style={{ padding: "0 10%" }}>
-          Sign in
-        </Text>
-        <Form success={handleSuccess} />
-      </ContentWrapper>
-    </Main>
+    <ContentWrapper>
+      <Text variant="h1" align="center" style={{ padding: "0 10%" }}>
+        Sign in
+      </Text>
+      <Form success={handleSuccess} />
+    </ContentWrapper>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import theme, { GlobalStyle } from "./styles"
+import styled, { ThemeProvider } from "styled-components"
+import TopBar from "./components/TopBar"
 
-import { ThemeProvider } from "styled-components"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 100vw;
+  padding: 5rem;
+  box-sizing: border-box;
+  position: relative;
+`
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "homepage",
+        element: <App />,
+      },
+    ],
+  },
+])
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Main>
+        <TopBar />
+        <RouterProvider router={router} />
+      </Main>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
